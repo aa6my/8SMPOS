133d92efcc722b5a197bbdb82f30155ba9015adb
azimfakhri <afkhris@gmail.com>
2015-01-02 18:27:23 +0800

    Upload Windows 8 metro apps POS

---
+++ POS/js/winapphack.js
@@ -0,0 +1,30 @@
+﻿function WinAppHack() {
+
+    if (MSApp && MSApp.execUnsafeLocalFunction) {
+
+        // override jquery html method
+        var old_html = $.fn.html;
+        $.fn.html = function () {
+            var args = arguments;
+            var This = this;
+            var result = null;
+            //$(this).css('font-size', 24)
+            MSApp.execUnsafeLocalFunction(function () {
+                result = old_html.apply(This, args);
+            });
+            return result;
+        };
+
+        // override alert method
+        alert = function (msg, Closed) {
+            if (!Closed)
+                Closed = function () { };
+            navigator.notification.alert(
+            msg,
+            Closed,
+            'Message',
+            'OK'
+            );
+        };
+    }
+}
